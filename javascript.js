acbb843a858ebf1d01f146eaba3a07d5213d2d9b
Hidemi2008 <Gabrielhidemi2@gmail.com>
2024-10-23 09:50:17 -0400

    Complete parciant the javascript

--- javascript.js
+++ javascript.js
@@ -2,7 +2,7 @@ let num1 = 0
 let num2 = 0
 let operation = 0
 
-
+let n1 = document.querySelector("#n1")
 let n2 = document.querySelector("#n2")
 let n3 = document.querySelector("#n3")
 let n4 = document.querySelector("#n4")
@@ -12,10 +12,15 @@ let n7 = document.querySelector("#n7")
 let n8 = document.querySelector("#n8")
 let n9 = document.querySelector("#n9")
 
+let suma = document.querySelector("#sum")
+let less = document.querySelector("#less")
+let mult = document.querySelector("#mul")
+let divi = document.querySelector("#division")
+console.log(suma)
 
+let inputE = document.querySelector('#entrada')
 
-
-console.log(inputE.value)
+let block = document.querySelector('#div')
 
 function sum(num = []){
     let soma = 0
@@ -53,33 +58,188 @@ function division(num = []){
 }
 
 
-function operate(operator,num1,num2){
+function operate(num1,operate,num2){
     if(operate == "+"){
         let arrayS = [num1,num2]
-        console.log(sum(arrayS))
+        return sum(arrayS)
     }else{
         if(operate == "-"){
             let arrayD = [num1,num2]
-            console.log(dif(arrayD))
+            return dif(arrayD)
         }else{
             if(operate == "*"){
-                let arrayM = [1,2,3]
-                console.log(mul(arrayM))
+                let arrayM = [num1,num2]
+               return mul(arrayM)
             }else{
                 if(operate == "/"){
-                    let arrayDiv = [8,4]
-                    console.log(division(arrayDiv))
+                    let arrayDiv = [num1,num2]
+                    return division(arrayDiv)
                 }
             }
         }
     }
 }
-let n1 = document.querySelector("#n1")
-let inputE = document.querySelector('#entrada')
+let fistN = 0
+let secondN = 0
 let num = 0
 let div = document.querySelector('div')
+
 // fuctions onclick in numbers
-n1.addEventListener('click',function(){ 
+function nu1(){
     num = 1
-    inputE.value.innerHTML = num
-})
+    n1.textContent = num
+    block.textContent = num
+    if(fistN == 0){
+        return fistN = num
+    }else{
+        if(secondN == 0){
+            return secondN = num
+        }
+    }
+}
+
+function nu2(){
+    num = 2
+    n2.textContent = num
+    block.textContent = num
+    if(fistN == 0){
+        return fistN = num
+    }else{
+        if(secondN == 0){
+            return secondN = num
+        }
+    }
+}
+
+function num3(){
+    num = 3
+    n3.textContent = num
+    block.textContent = num
+    if(fistN == 0){
+        return fistN = num
+    }else{
+        if(secondN == 0){
+            return secondN = num
+        }
+    }
+}
+
+function num4(){
+    num = 4
+    n4.textContent = num
+    block.textContent = num
+    if(fistN == 0){
+        return fistN = num
+    }else{
+        if(secondN == 0){
+            return secondN = num
+        }
+    }
+}
+
+function num5(){
+    num = 5
+    n5.textContent = num
+    block.textContent = num
+    if(fistN == 0){
+        return fistN = num
+    }else{
+        if(secondN == 0){
+            return secondN = num
+        }
+    }
+}
+
+function num6(){
+    num = 6
+    n6.textContent = num
+    block.textContent = num
+    if(fistN == 0){
+        return fistN = num
+    }else{
+        if(secondN == 0){
+            return secondN = num
+        }
+    }
+}
+
+function num7(){
+    num = 7
+    n7.textContent = num
+    block.textContent = num
+    if(fistN == 0){
+        return fistN = num
+    }else{
+        if(secondN == 0){
+            return secondN = num
+        }
+    }
+}
+
+function num8(){
+    num = 8
+    n8.textContent = num
+    block.textContent = num
+    if(fistN == 0){
+        return fistN = num
+    }else{
+        if(secondN == 0){
+            return secondN = num
+        }
+    }
+}
+
+function num9(){
+    num = 9
+    n9.textContent = num
+    block.textContent = num
+    if(fistN == 0){
+        return fistN = num
+    }else{
+        if(secondN == 0){
+            return secondN = num
+        }
+    }
+}
+let simbol = ""
+let ope = ""
+function s(){
+    simbol = "+"
+    suma.textContent = simbol
+    block.textContent = simbol
+    if(ope == ""){
+        return ope = simbol
+    }
+}
+
+function l(){
+    simbol = "-"
+    less.textContent = simbol
+    block.textContent = simbol
+    if(ope == ""){
+        return ope = simbol
+    }
+}
+
+function m(){
+    simbol = "*"
+    mult.textContent = simbol
+    block.textContent = simbol
+    if(ope == ""){
+        return ope = simbol
+    }
+}
+
+function d(){
+    simbol = "/"
+    divi.textContent = simbol
+    block.textContent = simbol
+    if(ope == ""){
+        return ope = simbol
+    }
+}
+let result = 0
+function work(){
+    result = operate(fistN,ope,secondN)
+    block.textContent = "This is result of account"
+}
